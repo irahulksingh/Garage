d52258977508eb8d982585b7d1b4438aeaedcb01
ayman alafandy <myprogram@barid.com>
2017-11-09 15:11:18 +0100

    Ayman

--- Garage/UserInterface.cs
+++ Garage/UserInterface.cs
@@ -11,6 +11,9 @@ namespace Garage
     {
         public void mainmenu()
         {
+            Car interfaceCar = new Car("Ayman", 111, "MAN", 222);
+            //Garage<Car> garage = new ();
+
             while (true)
             {
                 Console.Clear();
@@ -34,6 +37,7 @@ namespace Garage
                 }
             }
         }
+
         /// <summary>
         /// FUNCTION/METHOD TO CREATE A GARAGE NAME AND SIZE 
         /// </summary>
@@ -79,7 +83,7 @@ namespace Garage
                 }
             }
         }
-    
+
         /// <summary>
         /// THIS IS TO ADD OR REMOVE THINGS
         /// </summary>
@@ -88,16 +92,62 @@ namespace Garage
         {
             Console.Clear();
 
-            Motorcycle moto = new Motorcycle("harley", 2, "petrol", 1);
-            Garage<Vehicles> Addnew = new Garage<Vehicles>(moto);
-            Addnew.AddVehicle(moto);
-            foreach (var i in Addnew)
+            while (true)
             {
-                Console.WriteLine(i.GarageInfo());
+                Console.WriteLine("USE + TO ADD AND\n" +
+                                  "USE - TO REMOVE\n" +
+                                  "USE 0 TO EXIT TO MAIN MENU\n"+
+                                  "USE 9 TO CLEAN THE WINDOW");
+                string switchinput01 = Console.ReadLine();
+                switch (switchinput01)
+                {
+                    case "+":
+                        Console.WriteLine("TYPE 1 TO ADD CAR\n" +
+                                      "TYPE 2 TO ADD BUS\n" +
+                                      "TYPE 3 TO ADD MOTORCYCLE\n" +
+                                      "TYPE 4 TO ADD BOAT\n" +
+                                      "TYPE 5 TO ADD AIRPLANE\n" +
+                                      "TYPE 0 TO EXIT TO MAIN MENU\n" +
+                                      "TYPE 9 TO CLEAN THE WINDOW");
+                        string input = Console.ReadLine();
+                        Console.WriteLine("YOU CHOSE {0}\n ", input);
+                        switch (input)
+                        {
+
+                            case "1": Console.WriteLine("YOU ARE WORKING WITH 1 CAR\n"); break;
+                            case "2": Console.WriteLine("YOU ARE WORKING WITH 2 BUS\n"); break;
+                            case "3": Console.WriteLine("YOU ARE WORKING WITH 3 MOTORCYCLE\n"); break;
+                            case "4": Console.WriteLine("YOU ARE WORKING WITH 4 BOAT\n"); break;
+                            case "5": Console.WriteLine("YOU ARE WORKING WITH 5 AIRPLANE\n"); break;
+                            case "0": Console.WriteLine("RETURN ONE STEP BACK\n"); return;
+                            case "9": Console.WriteLine("CLEAN THE WINDOW"); Console.Clear(); break;
+                            default: Console.WriteLine("INVALID INPUT! ENTER SOMETHING FROM OPTIONS\n"); break;
+
+
+                        }
+                        break;
+
+                    case "-":
+                        Console.WriteLine("!!!YOU ARE REMOVEING SOMETHING NOW!!!\n");
+                        break;  
+                    case "0":
+                        return;
+                    case "9":
+                        Console.Clear();
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+
+
+
             }
-            //Console.WriteLine("");
-            Console.ReadLine();
+
         }
+
 
         /// <summary>
         /// WHAT IS THE POINT OF THIS METHOD???
@@ -108,5 +158,18 @@ namespace Garage
 
         }
 
+        //Motorcycle moto = new Motorcycle("harley", 2, "petrol", 1);
+        //Garage<Vehicles> Addnew = new Garage<Vehicles>(moto);
+        //Addnew.AddVehicle(moto);
+        //foreach (var i in Addnew)
+        //{
+        //    Console.WriteLine(i.GarageInfo());
+        //}
+        //Console.WriteLine("");
+        //Console.ReadLine();
     }
+
+   
+
+
 }
